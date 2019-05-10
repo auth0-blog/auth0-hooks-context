fced769d52207a5c4910fef0e04571c9d5f46361
Bruno Krebs <bruno.krebs@auth0.com>
2019-05-09 22:18:55 -0300

    ### Implement the `useReducer` hook

--- src/App.js
+++ src/App.js
@@ -14,11 +14,49 @@ const initialState = {
   }
 };
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'subscribeUser':
+      return {
+        ...state,
+        attendees: [...state.attendees, action.payload],
+        subscribed: true
+      };
+    case 'unSubscribeUser':
+      return {
+        ...state,
+        attendees: state.attendees.filter(
+          attendee => attendee !== action.payload
+        ),
+        subscribed: false
+      };
+    default:
+      return state;
+  }
+};
+
+const MeetupContextProvider = ({ user, ...props }) => {
+  const [state, dispatch] = React.useReducer(reducer, initialState.meetup);
+  return (
+    <MeetupContext.Provider
+      value={{
+        ...state,
+        handleSubscribe: () =>
+          dispatch({ type: 'subscribeUser', payload: user.name }),
+        handleUnSubscribe: () =>
+          dispatch({ type: 'unSubscribeUser', payload: user.name })
+      }}
+    >
+      {props.children}
+    </MeetupContext.Provider>
+  );
+};
+
 const App = () => (
   <UserContext.Provider value={initialState.user}>
     <UserContext.Consumer>
       {user => (
-        <MeetupContext.Provider value={initialState.meetup}>
+        <MeetupContextProvider user={user}>
           <MeetupContext.Consumer>
             {meetup => (
               <div>
@@ -29,11 +67,22 @@ const App = () => (
                   {meetup.attendees.map(attendant => (
                     <li>{attendant}</li>
                   ))}
+                  <p>
+                    {!meetup.subscribed ? (
+                      <button onClick={meetup.handleSubscribe}>
+                        Subscribe
+                      </button>
+                    ) : (
+                      <button onClick={meetup.handleUnSubscribe}>
+                        Unsubscribe
+                      </button>
+                    )}
+                  </p>
                 </div>
               </div>
             )}
           </MeetupContext.Consumer>
-        </MeetupContext.Provider>
+        </MeetupContextProvider>
       )}
     </UserContext.Consumer>
   </UserContext.Provider>
